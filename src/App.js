eccca8b03098b87a8928acfffeadb939aa4cc5e6
PKBAJPAI12 <prateek.cse.uiet@gmail.com>
2023-07-29 00:06:42 +0530

    design login page and img carousel slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Signup from './Components/SignUp';
+import Login from './Components/Login';
 import { BrowserRouter } from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
-    <Signup/>
+    <Login/>
     </BrowserRouter>
   );
 }
